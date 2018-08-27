9a772371a1aea76d2296dd57e07f68fc6dde77cf
liamsgotgenes <nowaczykliam@gmail.com>
2018-08-27 18:52:34 -0500

    Added vim-plug script

---
+++ vimplug.sh
@@ -0,0 +1,4 @@
+#installs vim-plug
+#https://github.com/junegunn/vim-plug
+~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
